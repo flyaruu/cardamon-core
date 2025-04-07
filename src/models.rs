d2f03f6e0f2c247d64f3590283fcf6d960e0faed
Frank Lyaruu <flyaruu@gmail.com>
2025-04-07 09:15:52 +0200

    docker cpu stats are not a percentage I think

--- src/models.rs
+++ src/models.rs
@@ -9,6 +9,8 @@ fn boa_model(a: f64, b: f64, c: f64, d: f64) -> impl Fn(f64) -> f64 {
     move |workload| a * (b * (workload + c)).ln() + d
 }
 
+const CORE_COUNT: f64 = 4.0;
+
 pub fn rab_model(metrics: &Vec<&Metrics>, power: &Power, ci_g_wh: f64) -> Data {
     let data = metrics
         .iter()
@@ -25,17 +27,24 @@ pub fn rab_model(metrics: &Vec<&Metrics>, power: &Power, ci_g_wh: f64) -> Data {
                 }
 
                 Power::Tdp(tdp) => {
-                    let delta_t_h = (x.time_stamp - y.time_stamp) as f64 / 3_600_000.0;
-
+                    
+                    let delta_t_millis = (x.time_stamp - y.time_stamp) as f64;
+                    let delta_t_h = delta_t_millis / 3_600_000.0;
                     // taking the midpoint of the two datapoints and dividing by 50 because we're
                     // assuming tdp is at 50% utilization
-                    (0.5 * (x.cpu_usage + y.cpu_usage)) / 50.0 * tdp * delta_t_h
+
+                    // I don't think cpu_usage is a percentage
+                    let avg_cpu = 0.5 * (x.cpu_usage + y.cpu_usage);
+                    // println!("Process1:: {} Process2:: {} Delta_t: {} avg_cpu: {}",x.process_name,y.process_name, delta_t_millis,avg_cpu);
+                    println!("millis: {} name: {} - {} - avg: {}", delta_t_millis,x.process_name,y.process_name, avg_cpu);
+                    avg_cpu / 0.5 * tdp * delta_t_h / CORE_COUNT
                 }
             }
         })
         .collect_vec();
 
-    let pow_w = data.iter().fold(0_f64, |x, acc| x + acc);
+    let pow_w = data.iter().fold(0_f64, |acc,x| x + acc);
+    println!("Total power: {}", pow_w);
     let co2_g_wh = pow_w * ci_g_wh;
 
     Data {
