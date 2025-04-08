46ebff9b566f19d09180f4dbfd4a2b2850de4d8d
Frank Lyaruu <flyaruu@gmail.com>
2025-04-08 10:55:13 +0200

    clean up core count

--- src/models.rs
+++ src/models.rs
@@ -9,8 +9,6 @@ fn boa_model(a: f64, b: f64, c: f64, d: f64) -> impl Fn(f64) -> f64 {
     move |workload| a * (b * (workload + c)).ln() + d
 }
 
-const CORE_COUNT: f64 = 4.0;
-
 pub fn rab_model(metrics: &Vec<&Metrics>, power: &Power, ci_g_wh: f64) -> Data {
     let data = metrics
         .iter()
@@ -25,9 +23,8 @@ pub fn rab_model(metrics: &Vec<&Metrics>, power: &Power, ci_g_wh: f64) -> Data {
                     // boa_model(a, b, c, d)(cpu_util * delta_t_h)
                     boa_model(a, b, c, d)(cpu_util) * delta_t_h
                 }
-
+                
                 Power::Tdp(tdp) => {
-                    
                     let delta_t_millis = (x.time_stamp - y.time_stamp) as f64;
                     let delta_t_h = delta_t_millis / 3_600_000.0;
                     // taking the midpoint of the two datapoints and dividing by 50 because we're
@@ -37,7 +34,8 @@ pub fn rab_model(metrics: &Vec<&Metrics>, power: &Power, ci_g_wh: f64) -> Data {
                     let avg_cpu = 0.5 * (x.cpu_usage + y.cpu_usage);
                     // println!("Process1:: {} Process2:: {} Delta_t: {} avg_cpu: {}",x.process_name,y.process_name, delta_t_millis,avg_cpu);
                     println!("millis: {} name: {} - {} - avg: {}", delta_t_millis,x.process_name,y.process_name, avg_cpu);
-                    avg_cpu / 0.5 * tdp * delta_t_h / CORE_COUNT
+                    // I think we also need to divide by the core count
+                    avg_cpu / 0.5 * tdp * delta_t_h / x.cpu_core_count as f64
                 }
             }
         })
